429f3bb5cc7c89173dbae66c9a9482800d7b4e46
Tom Oddy <tom_oddy@live.co.uk>
2025-08-05 21:56:46 +0100

    Moved request to server side

--- Blinker/Pages/Index.cshtml.cs
+++ Blinker/Pages/Index.cshtml.cs
@@ -1,20 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
 
 namespace Blinker.Pages
 {
+    [ValidateAntiForgeryToken]
     public class IndexModel : PageModel
     {
         public string Path = "http://192.168.1.169:7000/led";
+
+        public HttpClient Client { get; set; }
 
         public List<string> Colours { get; set; }
 
         public IndexModel()
         {
-            HttpClient client = new();
-            Colours = JsonConvert.DeserializeObject<ColourSet>(client.SendAsync(new HttpRequestMessage(HttpMethod.Get, Path)).Result.Content.ReadAsStringAsync().Result)?.Colours!;
+            Client = new();
+            Colours = JsonConvert.DeserializeObject<ColourSet>(Client.SendAsync(new HttpRequestMessage(HttpMethod.Get, Path)).Result.Content.ReadAsStringAsync().Result)?.Colours!;
         }
 
-        public void OnGet() { }
+        public async void OnGet(string? colour)
+        {
+            if (colour is not null)
+            {
+                await Client.SendAsync(new HttpRequestMessage(HttpMethod.Get, Path + "?colour=" + colour));
+            }
+        }
     }
 }
--- Blinker/Program.cs
+++ Blinker/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddAntiforgery(o => o.HeaderName = "XSRF-TOKEN");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
